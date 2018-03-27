75ac07931f4df7ca3752f51f2ad33154a76b1543
aleksandr minchiu <92sasha92@gmail.com>
2018-03-27 17:33:06 +0300

    main change

--- EX1/main.cpp
+++ EX1/main.cpp
@@ -1,8 +1,6 @@
 #include "RPS.h"
 
 int main(int argc, char *argv[]) {
-    string player_1_name_ = "player1.rps_board";
-    string player_2_name_ = "player2.rps_board";
 		RPS rps;
 		rps.Parser(1);
 		rps.Parser(2);
