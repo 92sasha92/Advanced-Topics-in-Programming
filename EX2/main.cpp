648f5532e4fcdedee5814349fd0a2f8e162da213
aleksandr minchiu <92sasha92@gmail.com>
2018-05-08 21:14:59 +0300

    merged

--- EX2/main.cpp
+++ EX2/main.cpp
@@ -9,10 +9,11 @@
 int main() {
     FileAlgorithm alg1, alg2;
     AutoAlgorithm alg3;
-    unique_ptr<PlayerAlgorithm> alg_3 = make_unique<AutoAlgorithm>();
+    //unique_ptr<PlayerAlgorithm> alg_3 = make_unique<AutoAlgorithm>();
     unique_ptr<PlayerAlgorithm> alg_2 = make_unique<FileAlgorithm>();
+    unique_ptr<PlayerAlgorithm> alg_1 = make_unique<FileAlgorithm>();
 //    unique_ptr<PlayerAlgorithm> alg_3 = make_unique<AutoAlgorithm>();
-    GameManager manager(std::move(alg_3), std::move(alg_2));
+    GameManager manager(std::move(alg_1), std::move(alg_2));
 //    RPS rps;
 //    EndOfGameHandler endOfGameHandler;
 //    vector<unique_ptr<PiecePosition>> vectorToFill;
