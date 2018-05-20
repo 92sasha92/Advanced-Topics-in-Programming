75157c05b1cb1ec48889e5a081f7e73e3c261eae
aleksandr minchiu <92sasha92@gmail.com>
2018-05-20 13:38:05 +0300

    tests changed

--- EX2/run_tests.sh
+++ EX2/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 err[53]=0
 counter=0
-for ((i=1; i<=52; i++ ))
+for ((i=1; i<=55; i++ ))
 do
     echo ""
     echo ""
@@ -17,7 +17,7 @@ do
     cp "./tests/test_$i/player1.rps_moves" "./player1.rps_moves"
     cp "./tests/test_$i/player2.rps_moves" "./player2.rps_moves"
 
-    ./ex2
+    ./ex2 file-vs-file
 
     if diff ./rps.output ./tests/test_$i/rps.output; then
         echo PASS
