2da40d008830ec000c843da26975e5ac9737ea9e
aleksandr minchiu <92sasha92@gmail.com>
2018-06-06 12:14:19 +0300

    include header added

--- EX3/AlgorithmRegistration.cpp
+++ EX3/AlgorithmRegistration.cpp
@@ -1,4 +1,5 @@
 #include "TournamentManager.h"
+#include "AlgorithmRegistration.h"
 
 AlgorithmRegistration::AlgorithmRegistration(std::string id, std::function<std::unique_ptr<PlayerAlgorithm>()> factoryMethod) {
     TournamentManager::getInstance().registerAlgorithm(id, factoryMethod);
